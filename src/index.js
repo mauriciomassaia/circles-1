3bd6f0d5da22a4bb24c5c800f1c3bfe9c5cde394
Mauricio Massaia <mauriciomassaia@gmail.com>
2020-03-28 23:04:01 +1100

    Update libraries and interaction

--- src/index.js
+++ src/index.js
@@ -11,9 +11,12 @@ const COLORS = [
   0xBDB4F0,
   0xC9BFBF
 ]
-
 const PI2 = Math.PI * 2
-
+const mouse = { x: 0, y: 0 }
+const center = {
+  x: window.innerWidth >> 1,
+  y: window.innerHeight >> 1
+}
 const app = new Application({
   width: window.innerWidth,
   height: window.innerHeight,
@@ -21,40 +24,34 @@ const app = new Application({
   antialias: true,
   backgroundColor: 0xf3f3f3
 })
-
-let frame = 0
-let x = window.innerWidth >> 1
-let y = window.innerHeight >> 1
-let curColor
-let container = new ParticleContainer()
-let mousePressed = false
-let speed = 1
-app.stage.addChild(container)
-
+const container = new ParticleContainer()
 const list = []
 
-function draw () {
-  if (mousePressed) {
-    speed += 0.25
-  } else {
-    speed -= 1
-  }
+let frame = 0
+let curColor
+let mousePressed = false
+let speed = 1
+let scale
 
-  speed = Math.min(20, Math.max(1, speed))
+app.stage.addChild(container)
+
+function draw () {
+  speed += mousePressed ? 0.025 : -1
+  speed = Math.min(10, Math.max(2, speed))
 
   curColor = COLORS[((frame / 100) >> 0) % COLORS.length]
 
   for (let i = 0; i < speed / 10; i += 1) {
-    createCircle(x, y, curColor, 32)
+    createCircle(curColor, 32)
     frame++
   }
 
   list.forEach(p => {
-    p.x += p.vx
-    p.y += p.vy
+    p.x += p.vx * speed * mouse.x
+    p.y += p.vy * speed * mouse.y
     p.time += 1
-    const s = 1 - (p.time / 190)
-    p.scale.set(s, s)
+    scale = 1 - (p.time / (190))
+    p.scale.set(scale, scale)
   })
 
   do {
@@ -66,22 +63,34 @@ function draw () {
   } while (list.length > 0)
 }
 
-function createCircle (x, y, c, h = 100) {
+function createCircle (c, h = 100) {
   const p = Sprite.from(ParticleImage)
   container.addChild(p)
-  p.position.set(x, y)
+  p.position.set(center.x, center.y)
+  p.anchor.set(0.5, 0.5)
   p.width = p.height = h
   p.tint = c
   p.time = 0
   p.vx = Math.cos(Math.random() * PI2) * speed
   p.vy = Math.sin(Math.random() * PI2) * speed
+
+  if (mousePressed) {
+    p.vx *= 0.7 - mouse.x
+    p.vy *= 0.7 - mouse.y
+  }
+
   list.push(p)
-  return p
 }
 
 window.addEventListener('mousedown', () => {
   mousePressed = true
 })
+
+window.addEventListener('mousemove', ({ clientX, clientY }) => {
+  mouse.x = clientX / window.innerWidth - 0.5
+  mouse.y = clientY / window.innerHeight - 0.5
+})
+
 window.addEventListener('mouseup', () => {
   mousePressed = false
 })
